d14d6225757e26d1b9a4cfd376a99adb62dc1ddd
chokominto159753-pixel <chokominto0906@gmail.com>
2026-06-25 23:22:04 +0900

    Add manifest.js for web app configuration

---
+++ mainfest.js
@@ -0,0 +1,21 @@
+{
+  "short_name": "文わけ",
+  "name": "文わけ文 - 知的な文章アーカイブ",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "icon-512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "./index.html",
+  "background_color": "#FAF8F5",
+  "theme_color": "#1C382F",
+  "display": "standalone",
+  "orientation": "portrait"
+}
